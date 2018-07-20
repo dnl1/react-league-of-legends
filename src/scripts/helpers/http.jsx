3fb3f20dd36772336b7aae8836578d1abc570cbf
Danilo Oliveira Gomes <danilooliveira9@hotmail.com>
2018-07-20 00:08:31 -0300

    http to https

--- src/scripts/helpers/http.jsx
+++ src/scripts/helpers/http.jsx
@@ -6,7 +6,7 @@ class Http {
             'Content-Type': 'application/json; charset=utf-8'
         }
         this._instance = axios.create({
-            baseURL: process.env.REACT_APP_API_BASE_URL || 'http://league-of-legends-api.herokuapp.com/',
+            baseURL: process.env.REACT_APP_API_BASE_URL || 'https://league-of-legends-api.herokuapp.com/',
             timeout: process.env.REACT_APP_API_TIMEOUT || 10000,
             headers: {
             }
