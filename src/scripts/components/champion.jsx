630a03ece6c242c8832926e4e1aa4874cefd0fa4
Tiago Lima <tiago.t0871.beyond@xpi.com.br>
2018-08-03 18:04:05 -0300

    ajustes de layout e animação de entrada dos items

--- src/scripts/components/champion.jsx
+++ src/scripts/components/champion.jsx
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 class Champion extends Component {
     render() {
         return (
-            <div className="champion">
-                <img className="champion__image" alt={this.props.name} src={`//ddragon.leagueoflegends.com/cdn/8.13.1/img/champion/${this.props.chave.replace('\'','').replace(' ','')}.png`} width="84"/>
-                <section>
-                    <p>{this.props.name}</p>
-                </section>
-            </div>
+            <ReactCSSTransitionGroup
+                transitionName="fade"
+                transitionAppear={true}
+                transitionAppearTimeout={500}
+                transitionLeave={true}
+                transitionLeaveTimeout={500}>
+
+                <div className="champion">
+                    <img className="champion__image" alt={this.props.name} src={`//ddragon.leagueoflegends.com/cdn/8.13.1/img/champion/${this.props.chave.replace('\'','').replace(' ','')}.png`} width="84"/>
+                    <section>
+                        <p>{this.props.name}</p>
+                    </section>
+                </div>
+            </ReactCSSTransitionGroup>
         );
     }
 }
