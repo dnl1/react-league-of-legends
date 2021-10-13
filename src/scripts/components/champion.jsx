6b49f876347af3849d2d9ba60a2431448d0f0817
Danilo Gomes <danilooliveira9@hotmail.com>
2021-10-12 22:47:36 -0300

    fix

--- src/scripts/components/champion.jsx
+++ src/scripts/components/champion.jsx
@@ -23,7 +23,7 @@ class Champion extends Component {
 
                 <Link to={routeHelper.resolvePath(`/champions/${this.props.name.replace('.', '').toLowerCase()}`)}>
                     <div className="champion">
-                        <img className="champion__image" onLoad={this.onImageLoad.bind(this)} alt="" src={`//ddragon.leagueoflegends.com/cdn/11.20.1/img/champion/${this.props.id.png}`} width="84" />
+                        <img className="champion__image" onLoad={this.onImageLoad.bind(this)} alt="" src={`//ddragon.leagueoflegends.com/cdn/11.20.1/img/champion/${this.props.id}.png`} width="84" />
                         <section>
                             <p>{this.props.name}</p>
                         </section>
